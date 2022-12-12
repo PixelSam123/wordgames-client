523532ad97ff146ecf302492a16237c05784eb96
PixelSam123 <77851256+PixelSam123@users.noreply.github.com>
2022-12-12 12:47:31 +0700

    push messages in main thread instead of reader thread

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 use std::{
     net::TcpStream,
-    sync::{Arc, Mutex},
+    sync::{
+        mpsc::{self, Receiver},
+        Arc, Mutex,
+    },
     thread,
 };
 
@@ -43,35 +46,46 @@ fn main() {
 
 fn connect(
     ctx: egui::Context,
-    messages: Arc<Mutex<Vec<String>>>,
-) -> Result<Arc<Mutex<WebSocket<MaybeTlsStream<TcpStream>>>>, String> {
+) -> Result<
+    (
+        Arc<Mutex<WebSocket<MaybeTlsStream<TcpStream>>>>,
+        Receiver<String>,
+    ),
+    String,
+> {
     let (socket, _) =
         tungstenite::connect("ws://localhost:8080/ws/monka").map_err(|err| err.to_string())?;
     let socket = Arc::new(Mutex::new(socket));
 
+    let (message_tx, message_rx) = mpsc::channel();
+
     let reader_socket = socket.clone();
     thread::spawn(move || loop {
         let msg = reader_socket.lock().unwrap().read_message().unwrap();
-        messages.lock().unwrap().push(msg.to_string());
+        message_tx.send(msg.to_string()).unwrap();
 
         ctx.request_repaint();
     });
 
-    Ok(socket)
+    Ok((socket, message_rx))
 }
 
 #[derive(Default)]
 struct WordgamesClient {
     ws: Option<Arc<Mutex<WebSocket<MaybeTlsStream<TcpStream>>>>>,
     err_text: Option<String>,
-    messages: Arc<Mutex<Vec<String>>>,
+    messages: Vec<String>,
+    message_rx: Option<mpsc::Receiver<String>>,
     message_to_send: String,
 }
 
 impl WordgamesClient {
     fn connect_button_clicked(&mut self, ctx: &egui::Context) {
-        match connect(ctx.clone(), self.messages.clone()) {
-            Ok(ws) => self.ws = Some(ws),
+        match connect(ctx.clone()) {
+            Ok((ws, message_rx)) => {
+                self.ws = Some(ws);
+                self.message_rx = Some(message_rx);
+            }
             Err(err) => self.err_text = Some(err),
         }
     }
@@ -79,7 +93,13 @@ impl WordgamesClient {
     fn message_field_submitted(&mut self) {
         if let Some(ws) = &self.ws {
             // Deadlock here
-            ws.lock().unwrap().write_message(tungstenite::Message::Text(self.message_to_send.clone())).unwrap();
+            if let Err(err) = ws
+                .lock()
+                .unwrap()
+                .write_message(tungstenite::Message::Text(self.message_to_send.clone()))
+            {
+                self.err_text = Some(err.to_string());
+            }
         }
 
         self.message_to_send = String::new();
@@ -88,6 +108,14 @@ impl WordgamesClient {
 
 impl eframe::App for WordgamesClient {
     fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
+        // fetch message from reader thread
+        if let Some(message_rx) = &self.message_rx {
+            if let Ok(message) = message_rx.try_recv() {
+                self.messages.push(message);
+            }
+        }
+
+        // UI
         egui::CentralPanel::default().show(ctx, |ui| {
             ui.label(format!("{:?}", self.err_text));
 
@@ -106,7 +134,7 @@ impl eframe::App for WordgamesClient {
             }
 
             ui.heading("Messages: ");
-            self.messages.lock().unwrap().iter().for_each(|message| {
+            self.messages.iter().for_each(|message| {
                 ui.label(message);
             });
         });
